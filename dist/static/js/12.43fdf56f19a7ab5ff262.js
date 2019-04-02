96ed264ae2db2368b1e2ce7e7e04a1ecde294253
轻轻的小雨滴 <2102439652@qq.com>
2019-04-03 01:01:28 +0800

    删除dist输出目录

--- dist/static/js/12.43fdf56f19a7ab5ff262.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([12],{"7L3X":function(e,t){},FRs3:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var s={render:function(){var e=this.$createElement;return(this._self._c||e)("div",{staticClass:"wapper"},[this._v("\n\t用户绑定页面\n")])},staticRenderFns:[]};var a=n("VU/8")({},s,!1,function(e){n("7L3X")},"data-v-f57a1bc6",null);t.default=a.exports}});
