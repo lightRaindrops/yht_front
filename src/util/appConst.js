7745ff61bfecfee5a860d12c1a4ae712f9436b8a
轻轻的小雨滴 <2102439652@qq.com>
2019-04-03 00:48:01 +0800

    本地配置

--- src/util/appConst.js
+++ src/util/appConst.js
@@ -2,13 +2,13 @@
 let BACKEND_DOMAIN = 'https://e.yhtjc.com/v2/public/index.php/api/';
 let BACKEND_DOMAIN_VIEW_URL = 'https://e.yhtjc.com/v2/public/index.php/view/attr';
 
-if (process.env.NODE_ENV === 'development') {
-
-    BACKEND_DOMAIN = 'http://421037.f3322.net:8090/yhtsys/public/index.php/api/';
-   	//BACKEND_DOMAIN = 'http://421037.f3322.net:8090/index.php/api/';
-    //  BACKEND_DOMAIN = 'http://dev.miaoyi09.com/index.php/api/';
-    BACKEND_DOMAIN_VIEW_URL = 'http://dev.miaoyi09.com/index.php/view/attr';
-}
+// if (process.env.NODE_ENV === 'development') {
+//
+//     BACKEND_DOMAIN = 'http://421037.f3322.net:8090/yhtsys/public/index.php/api/';
+//    	//BACKEND_DOMAIN = 'http://421037.f3322.net:8090/index.php/api/';
+//     //  BACKEND_DOMAIN = 'http://dev.miaoyi09.com/index.php/api/';
+//     BACKEND_DOMAIN_VIEW_URL = 'http://dev.miaoyi09.com/index.php/view/attr';
+// }
 
 let EDITOR_IMG_URL = BACKEND_DOMAIN + 'editor/uploadimage';
 let FILE_UPLOAD_URL = BACKEND_DOMAIN + 'file/upload';
